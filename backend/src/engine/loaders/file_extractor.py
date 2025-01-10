f5f6c0ecbdbc6370bdc5fa2a94b0091f987c063f
mor <mordvash1@gmail.com>
2025-01-10 14:18:18 +0200

    add pipeline loaders

---
+++ backend/src/engine/loaders/file_extractor.py
@@ -0,0 +1,60 @@
+import logging
+from abc import ABC, abstractmethod
+from docx import Document as DocxDocument
+from fastapi import UploadFile
+from pypdf import PdfReader
+from src.utils.api_error_response import ApiErrorException
+from src.utils.logger import setup_logging
+from src.utils.status import HTTP_500_INTERNAL_SERVER_ERROR
+
+setup_logging()
+logger = logging.getLogger(__name__)
+
+
+class FileExtractor(ABC):
+
+    def __init__(self, file: UploadFile):
+
+        self.file = file
+
+    @abstractmethod
+    async def extract(self) -> str:
+        """
+        Abstract method to extract text from the file.
+        """
+
+
+class DocxExtractor(FileExtractor):
+
+    async def extract(self) -> str:
+        try:
+            logger.info("Processing DOCX file...")
+            doc = DocxDocument(self.file.file)
+            text = "\n".join([paragraph.text for paragraph in doc.paragraphs])
+            return text
+        except Exception as e:
+            raise ApiErrorException(error_code=HTTP_500_INTERNAL_SERVER_ERROR, message=f"Error processing DOCX file: {e}")
+
+
+class TxtExtractor(FileExtractor):
+    async def extract(self) -> str:
+        try:
+            logger.info("Processing text file...")
+            content = await self.file.read()
+            return content.decode("utf-8")
+        except Exception as e:
+            raise ApiErrorException(error_code=HTTP_500_INTERNAL_SERVER_ERROR, message=f"Error processing text file: {e}")
+
+
+class PdfExtractor(FileExtractor):
+
+    async def extract(self) -> str:
+        try:
+            logger.info("Processing PDF file...")
+            pdf_reader = PdfReader(self.file.file)
+            text = ""
+            for page in range(len(pdf_reader.pages)):
+                text += pdf_reader.pages[page].extract_text()
+            return text
+        except Exception as e:
+            raise ApiErrorException(error_code=HTTP_500_INTERNAL_SERVER_ERROR, message=f"Error processing PDF file: {e}")
