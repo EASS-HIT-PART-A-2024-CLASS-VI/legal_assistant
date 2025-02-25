46e87a662dc63b80f66dbdbd7f3e33aea8d2ff51
Mor Dvash <mordvash1@gmail.com>
2025-02-25 14:44:17 +0200

    add ci test

--- backend/src/env.py
+++ backend/src/env.py
@@ -5,7 +5,6 @@ config = SettingsConfigDict(env_file=".env", protected_namespaces=("settings_",)
 
 
 class Config(BaseSettings):
-    gemini_key: str = Field(..., description="API key for accessing the gemini API.")
     openai_api_key: str = Field(..., description="API key for accessing the OpenAI API.")
     falkordb_host: str = Field(default="localhost", description="Host for connecting to the FalkorDB instance.")
     falkordb_rag_uri: str = Field(description="URI for connecting to the FalkorDB instance.")
--- backend/tests/unit_tests/conftest.py
+++ backend/tests/unit_tests/conftest.py
@@ -4,7 +4,6 @@ import pytest
 @pytest.fixture
 def mock_env_vars(monkeypatch):
     monkeypatch.setenv("OPENAI_API_KEY", "test_openai_key")
-    monkeypatch.setenv("GEMINI_KEY", "test_gemini_key")
     monkeypatch.setenv("FALKORDB_PASSWORD", "test_password")
     monkeypatch.setenv("FALKORDB_RAG_URI", "test_uri")
     yield
